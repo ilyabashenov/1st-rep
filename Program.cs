1e5edd8281379cf4c2fc53abe2c7e1907fbb2c75
ilyabashenov <51296268+ilyabashenov@users.noreply.github.com>
2019-06-02 22:54:41 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,29 +11,29 @@ namespace ConsoleApp4
         {
 
 
-            string[] sprtr = { "\n", " ", ",", ".", ":", "\t", "/n" }; //Разделитель строк 
-            Dictionary<string, int> words = new Dictionary<string, int>(); // Словарь
+            string[] wordSeparators = { "\n", " ", ",", ".", ":", "\t", "/n" }; //Разделитель строк 
+            Dictionary<string, int> wordCointerDictionary = new Dictionary<string, int>(); // Словарь
 
             string path = @"C:\text.txt"; //Переменная с расположением файла            
-            using (StreamReader reader = new StreamReader(path, Encoding.UTF8))// Открываю файл через Using
-            {
+            //using (StreamReader reader = new StreamReader(path, Encoding.UTF8))// в using только получение ресурсов. И надо тогда через ридер
+            
                 string text = File.ReadAllText(path); // Сохраняю текст в строку
-                string[] words2 = text.Split(sprtr, System.StringSplitOptions.RemoveEmptyEntries); // Разделяю строку на массивы и удаляю пустые массивы
+                string[] words2 = text.Split(wordSeparators, System.StringSplitOptions.RemoveEmptyEntries); // Разделяю строку на массивы и удаляю пустые массивы
 
                 for (int i = 0; i < words2.Length; i++)
                 {
-                    words2[i]= words2[i].ToLower(); // Перевожу в нижний регистр, чтобы сравнение было корректным 
-                    if (!words.ContainsKey(words2[i])) // Если слова нет в словаре 
-                        words.Add(words2[i], 1); // Добавляет
-                    else words[words2[i]] += 1; // Иначе добавляет значение
+                    words2[i] = words2[i].ToLower(); // Перевожу в нижний регистр, чтобы сравнение было корректным 
+                    if (!wordCointerDictionary.ContainsKey(words2[i])) // Если слова нет в словаре 
+                    wordCointerDictionary.Add(words2[i], 1); // Добавляет
+                    else wordCointerDictionary[words2[i]] += 1; // Иначе добавляет значение
                 }
 
-                foreach (var word in words)
+                foreach (var word in wordCointerDictionary)
                     Console.Write($"{word} "); // Вывод информации
 
-              
 
-            }
+            
+            
         }
     }
 }
